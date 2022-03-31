db87f8eaac33a078f188d1dda6c274ed73c7e88e
VasuDanapana <vasu.java.techie@gmail.com>
2022-03-31 20:00:24 +0530

    initla commit1

--- src/main/java/com/jfsd/SpringJenkinsDockerIntegrationApplication.java
+++ src/main/java/com/jfsd/SpringJenkinsDockerIntegrationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJenkinsDockerIntegrationApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Testing............");
 		SpringApplication.run(SpringJenkinsDockerIntegrationApplication.class, args);
 	}
 
